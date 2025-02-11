ba567bac864bf31178948c069626b2960c965251
Allistair <github.a1@spicygalaxy.com>
2025-02-10 21:32:07 -0800

    11ty config update

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,4 +1,15 @@
 export default async function (eleventyConfig) {
   // Configure Eleventy
+  eleventyConfig.setInputDirectory("src");
+  eleventyConfig.setDataDirectory("_data");
+  eleventyConfig.setIncludesDirectory("_includes");
+  eleventyConfig.setLayoutsDirectory("_includes/layouts");
 
-};
+  eleventyConfig.setTemplateFormats("html, md, njk");
+
+  // Shortcode
+  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+
+  // Copy static assets
+  eleventyConfig.addPassthroughCopy("src/assets");
+}; 
